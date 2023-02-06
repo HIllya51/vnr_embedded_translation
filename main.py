ef6102df9363f0a5b62ac0edb03b373c3d688aeb
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2023-02-06 21:35:29 +0800

    1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from PySide.QtCore import QCoreApplication  
 from rpcman import RpcServer
 from gameagent import GameAgent
-import threading,json
+import threading,json,time
 
 import win32pipe, win32file,win32con,win32security
 PIPE_TEXT_EMBED_agent2host="\\\\.\\Pipe\\PIPE_TEXT_EMBED_agent2host"
@@ -14,6 +14,7 @@ from PySide.QtCore import Signal, Qt, QObject
 class qapp(QCoreApplication):
     end = Signal()
     pipe_agent2host=Signal(str)
+    connected=Signal()
     engineTextReceived_tohost=Signal(unicode, str,  int )
     def __init__(self,arg) :
         super(qapp,self).__init__(arg) 
@@ -26,12 +27,21 @@ class qapp(QCoreApplication):
         threading.Thread(target=self._listener).start() 
 
         print("namepipe_ok")
+        self.isconnect=False
         self.end.connect(self.quit)
         self.engineTextReceived_tohost.connect(self.transfer_to_host) 
+        self.connected.connect(self.connectget)
+        threading.Thread(target=self.waitforconnect).start()
+    def connectget(self ):
+        self.isconnect=True
+    def waitforconnect(self):
+        time.sleep(3)
+        self.send({"command":"no_connection"})
+        self.end.emit()
     def send(self,s):
         try: 
             print("send",s)
-            win32file.WriteFile(self.pipe_send,s.encode('utf8'))
+            win32file.WriteFile(self.pipe_send,json.dumps(s).encode('utf8'))
         except:
             print_exc()
     def _creater1(self):
@@ -44,7 +54,7 @@ class qapp(QCoreApplication):
     def transfer_to_host(self,text, hash, role ):
         lang='zhs' 
          
-        self.send(json.dumps({
+        self.send(({
             "command":"trans",
             "text":text,
             "hash":hash,
@@ -56,6 +66,7 @@ class qapp(QCoreApplication):
             while True:
                 rd=win32file.ReadFile(self.pipe_get, 65535, None)[1]  
                 rd=json.loads(rd)
+                print(type(rd))
                 print("received",rd)
                 self._onreceive_callback(rd) 
                 
@@ -77,7 +88,8 @@ if __name__=="__main__":
     app.ga.attachProcess(pid=int(sys.argv[1])) 
     engine=app.ga.guessEngine(pid=int(sys.argv[1]))
     if engine:
-        app.send(json.dumps({'command':"engine","name":engine.name},ensure_ascii=False))
+        app.send(({'command':"engine","name":engine.name} ))
     app.rpc.engineTextReceived.connect(app.engineTextReceived_tohost) 
     app.rpc.clearAgentTranslation() 
+    app.rpc.agentConnected.connect(app.connected)
     sys.exit(app.exec_())
