8f2c4a8f08badcc4bef759673be41a239755fece
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2023-02-06 11:44:01 +0800

    1

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 
 import time
 import sys
-from PySide.QtCore import QCoreApplication 
-from PySide.QtGui import QMainWindow,QApplication
+from PySide.QtCore import QCoreApplication  
 from rpcman import RpcServer
 from gameagent import GameAgent
+from threading import Thread
+
+from PySide.QtCore import Signal, Qt, QObject
+class qapp(QCoreApplication):
+    end = Signal()
+    def __init__(self,arg) :
+        super(qapp,self).__init__(arg)
+        self.end.connect(self.quit)
+        Thread(target=self.th).start()
+    def th(self):
+        while True:
+            a=raw_input()
+            if a=='quit':
+                self.end.emit()
+                break
 if __name__=="__main__":
-    app =  QApplication(sys.argv) 
+    app =  qapp(sys.argv) 
     rpc=RpcServer()
     rpc.start()   
     ga=GameAgent(rpc)
     ga.attachProcess(pid=int(sys.argv[1]))
-    rpc.engineTextReceived.connect(ga.sendEmbeddedTranslation)
-    rpc.clearAgentTranslation()
-    x=QMainWindow()
-    x.show()
+    rpc.engineTextReceived.connect(ga.sendEmbeddedTranslation)  
+    rpc.clearAgentTranslation() 
     sys.exit(app.exec_())
