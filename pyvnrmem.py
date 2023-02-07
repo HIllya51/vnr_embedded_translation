14bd65f4b19316dd7492d37f0d51dd1c366cc2f6
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2023-02-08 00:23:39 +0800

    1

--- pyvnrmem.py
+++ pyvnrmem.py
@@ -1,5 +1,5 @@
 
-from  PyQt5.QtCore import QSharedMemory,QObject
+from  PyQt4.QtCore import QSharedMemory,QObject
 from ctypes import Structure,c_int8,c_int64,c_int8,c_char,c_int32,c_wchar,c_char_p,c_wchar_p,POINTER,cast,byref,create_string_buffer,create_unicode_buffer
 LanguageCapacity=4
 import struct,ctypes 
@@ -41,7 +41,7 @@ class VnrSharedMemory(QObject):
         return cast(POINTER(self.memory.data().asarray() ),POINTER(Cell)).contents 
         
     def key(self):
-        self.memory.key()
+        return self.memory.key()
     def setKey(self,v):
         self.memory.setKey(v)
     
@@ -52,66 +52,74 @@ class VnrSharedMemory(QObject):
         return ok
     def attach(self,readOnly):
         return self.memory.attach(QSharedMemory.ReadOnly if readOnly else QSharedMemory.ReadWrite)
-    def detach(self):
+    def detach_(self):
         return self.memory.detach()
     def isAttached(self):
-        return self.memory.isAttached()
-    def lock(self):
-        return self.memory.lock()
-    def unlock(self):
-        return self.memory.unlock()
-    def errorString(self):
-        return self.memory.errorString()
-    def hasError(self):
-        return self.memory.error()!=QSharedMemory.NoError 
-    def setDataHash(self,i,v:int):
-        v=v.to_bytes(8,'little',signed=True)
-        mv=memoryview(self.memory.data()).cast('B') 
+        return self.memory.isAttached() 
+        
+    def setDataHash(self,i,v ): 
+        v=self.packuint(v,8) 
+        mv=memoryview(self.memory.data()) 
         for i in range(8):
-            mv[i+8]=v[i] 
-    def setDataStatus(self,i,v:int):
-        self.memory.lock()
-        v=v.to_bytes(8,'little')
-        mv=memoryview(self.memory.data()).cast('B') 
+            mv[i+8]=v[i]  
+    def packuint(self,i, size=0): # int -> str
+        """
+        @param  i  int
+        @param* size  int  total size after padding
+        @return  str
+        """
+        if i<0:
+            i=(2**(8*size))+i
+        r = ''
+        while i:
+            r = r+chr(i & 0xff) 
+            i = i >> 8
+        while len(r) < size:
+            r = r+chr(0) 
+        return r
+    def setDataStatus(self,i,v ):  
+        v=self.packuint(v,8)
+        mv=memoryview(self.memory.data()) 
         for i in range(8):
-            mv[i]=v[i] 
-        self.memory.unlock()
+            mv[i]=v[i]   
     def setDataRole(self,i,v):
-        v=v.to_bytes(1,'little')
-        mv=memoryview(self.memory.data()).cast('B') 
+        v=self.packuint(v,1)
+        mv=memoryview(self.memory.data()) 
         mv[16]=v[0]
+        print("role",v[0]) 
+        cache=[]
+        for i in range(100):
+            cache.append(ord(mv[i]))
+        print((cache) )
     def setDataLanguage(self,i,v):
         v=v.encode('ascii')
-        mv=memoryview(self.memory.data()).cast('B') 
+        mv=memoryview(self.memory.data()) 
         for i in range(min(8,len(v))):
             mv[i+17]=v[i] 
-    def setDataText(self,i,v):
-        v=v
-        uv=v.encode('utf-16-le')
-        self.memory.lock()
-        size=len(uv).to_bytes(4,'little')
-        mv=memoryview(self.memory.data()).cast('B')
-          
-        for i in range(min(self.memory.size(),100)):
-            print(mv[i],end=',')
-        print(mv[25])
+        print("lang")
         cache=[]
-        for i in range(mv[25]):
-            cache.append(mv[29+i])
-        print(bytes(cache).decode('utf-16-le'))
-        # w=create_unicode_buffer(v)
-        # self.cell(i).language=w
-
-         
+        for i in range(100):
+            cache.append(ord(mv[i]))
+        print((cache) )
+    def setDataText(self,i,v): 
+        l=len(v)
+        uv=v.encode('utf-16-le') 
+        v=self.packuint(l,4)
+        mv=memoryview(self.memory.data()) 
         for i in range(4):
-            mv[i+25]=size[i]
-        for i,b in enumerate(uv):
-            mv[i+29]=b
-        mv=memoryview(self.memory.data()).cast('B')
-        print(mv[25])
+            mv[i+24]=v[i] 
+
+        for i in range(len(uv)):
+            mv[i+28]=uv[i]
+            
         cache=[]
-        for i in range(mv[25]):
-            cache.append(mv[29+i])
-        print(bytes(cache).decode('utf-16-le'))
-        #mv[21]=
-        self.memory.unlock()
+        for i in range(100):
+            cache.append(ord(mv[i]))
+        print((cache) )
+        # cache=[]    
+        # print(ord(mv[24]))
+        # for i in range(ord(mv[24])):
+        #     cache.append(ord(mv[i]))
+
+        
+        #print((cache) )
